f816bfa954cd8bf8025e0468fccfd08e3dda1b70
Ruby Itimu Magella <160042683+RubyMagella@users.noreply.github.com>
2024-02-27 12:17:40 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+print("Hello world,This is awesome")
+site_name = 'Power Learn Project'
+print(site_name)
+site_name = 'apple.com'
+print(site_name)
+a,b,c = 5,3.5,'hello'
+print(a)
+print(b)
+print(c)
+site1 = site2 = 'Power Learn Project'
+print(site1)
+print(site2)
+name = 'Python'
+print(name)
+message = 'Python for beginners'
+print(message)
+name = 'Ruby Magella'
+print(name)
+print(5+6)
+print('Goodmorning,it is a rainy day')
+
+
